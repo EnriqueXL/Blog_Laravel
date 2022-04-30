082e1027d72322f9953ecd16da52d8210f18aa5d
Enrique <luiswetowkn@gmail.com>
2022-04-30 01:21:35 -0500

    Traducción a español

---
+++ lang/es/login-formulario.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'Login' => 'Acceso',
+    'Email Address' => 'Correo Electrónico',
+    'Password' => 'Contraseña',
+    'Remember Me' => 'Recuérdame',
+    'Forgot Your Password?' => 'Olvidaste tu contraseña?'
+
+];
---
+++ lang/es/registro-formulario.php
@@ -0,0 +1,8 @@
+<?php 
+return [
+    'Register' => 'Registro',
+    'Name' => 'Nombre',
+    'Email Address'=> 'Correo electronico',
+    'Password' => 'Contraseña',
+    'Confirm Password' => 'Confirmar contraseña'
+];
